62ddbd9a1f65f1a093283d866c56a8974a7bf474
morvayF <58747791+morvayF@users.noreply.github.com>
2024-02-26 21:26:17 +0100

    added dependencies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.commons.lang3.StringUtils;
+;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		if (StringUtils.isBlank("ddd")) {
+			System.out.println("documents");
+		}
+
 	}
 
 }
